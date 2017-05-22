7ff85f2dafe8a71eebeb7493d9058de19e681d2c
Brenna <bcmdarroch@gmail.com>
2017-05-22 15:53:38 -0700

    works w key strokes too

--- noise.js
+++ noise.js
@@ -1,9 +1,20 @@
 $(document).ready( function() {
-  $('.instrument').on ('click', '.note', function() {
+  $('.instrument').on('click', '.note', function() {
     var note = $(this).html();
     var audio = '#' + note + "Audio";
-    console.log(note);
+
     $(audio)[0].load();
     $(audio)[0].play();
+  });
+
+  $('body').on('keydown', function(e) {
+    var note = e.key;
+    var audio = '#' + note + "Audio";
+
+    if (['a', 'b', 'c', 'd', 'e', 'f', 'g'].includes(note)) {
+      $(audio)[0].load();
+      $(audio)[0].play();
+    }
+
   });
 });
