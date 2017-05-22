06518a7ec3c8434e6e9c96199cac087cacc15fa2
Kayla <kubicke.kayla@gmail.com>
2017-05-22 15:52:02 -0700

    Unique note is played for each button.
    User can click an individual button and a specific note will play.

--- noise.js
+++ noise.js
@@ -5,18 +5,39 @@ $(document).ready( function() {
   // Grabbing the audio source. (Working)
   var audioC = document.getElementById("cAudio");
   var audioD = document.getElementById("dAudio");
+  var audioE = document.getElementById("eAudio");
+  var audioF = document.getElementById("fAudio");
+  var audioG = document.getElementById("gAudio");
+  var audioA = document.getElementById("aAudio");
+  var audioB = document.getElementById("bAudio");
 
 
-  var playNote = function() {
-    audioC.play();
+  var playNote = function(note) {
+    if (note === 'c') {
+      audioC.play();
+    } else if (note === 'd') {
+      audioD.play();
+    } else if (note === 'e') {
+      audioE.play();
+    } else if (note === 'f') {
+      audioF.play();
+    } else if (note === 'g') {
+      audioG.play();
+    } else if (note === 'a') {
+      audioA.play();
+    } else if (note === 'b') {
+      audioB.play();
+    }
   };
 
-  // var pauseNote = function() {
-  //   audioC.pause();
-  // };
+  var pauseNote = function() {
+    audioC.pause();
+  };
 
   $('.instrument').on('click', 'button', function(event) {
     console.log(event);
-    $(this).addClass(playNote());
+    console.log($(this).html());
+    var note = $(this).html();
+    $(this).addClass(playNote(note));
   });
 });
