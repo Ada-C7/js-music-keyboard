4734b1ed442b9be44bb0e4ad3fc2bd2b31a29588
Kelly Souza <kellysouza99@gmail.com>
2017-05-22 15:13:49 -0700

    key press 1-7 work

--- noise.js
+++ noise.js
@@ -1,49 +1,62 @@
 $(document).ready(function() {
 
-  var audioElementC = document.createElement('audio');
-    audioElementC.setAttribute('src', 'media/c_note.mp3');
-
-  var audioElementD = document.createElement('audio');
-    audioElementD.setAttribute('src', 'media/d_note.mp3');
-
-  var audioElementE = document.createElement('audio');
-    audioElementE.setAttribute('src', 'media/e_note.mp3');
-
-  var audioElementF = document.createElement('audio');
-    audioElementF.setAttribute('src', 'media/f_note.mp3');
-
-  var audioElementG = document.createElement('audio');
-    audioElementG.setAttribute('src', 'media/g_note.mp3');
-
-  var audioElementA = document.createElement('audio');
-    audioElementA.setAttribute('src', 'media/a_note.mp3');
-
-  var audioElementB = document.createElement('audio');
-    audioElementB.setAttribute('src', 'media/b_note.mp3');
-
-
+  // var audioElementC = document.createElement('audio');
+  //   audioElementC.setAttribute('src', 'media/c_note.mp3');
+  //
+  // var audioElementD = document.createElement('audio');
+  //   audioElementD.setAttribute('src', 'media/d_note.mp3');
+  //
+  // var audioElementE = document.createElement('audio');
+  //   audioElementE.setAttribute('src', 'media/e_note.mp3');
+  //
+  // var audioElementF = document.createElement('audio');
+  //   audioElementF.setAttribute('src', 'media/f_note.mp3');
+  //
+  // var audioElementG = document.createElement('audio');
+  //   audioElementG.setAttribute('src', 'media/g_note.mp3');
+  //
+  // var audioElementA = document.createElement('audio');
+  //   audioElementA.setAttribute('src', 'media/a_note.mp3');
+  //
+  // var audioElementB = document.createElement('audio');
+  //   audioElementB.setAttribute('src', 'media/b_note.mp3');
 
 
-  $('.instrument').on('click', 'button.c', function(event) {
-      audioElementC.load(); audioElementC.play();
-  });
-  $('.instrument').on('click', 'button.d', function(event) {
-      audioElementD.load(); audioElementD.load(); audioElementD.play();
-  });
-  $('.instrument').on('click', 'button.e', function(event) {
-      audioElementE.load(); audioElementE.play();
-  });
-  $('.instrument').on('click', 'button.f', function(event) {
-      audioElementF.load(); audioElementF.play();
-  });
-  $('.instrument').on('click', 'button.g', function(event) {
-      audioElementG.load(); audioElementG.play();
-  });
-  $('.instrument').on('click', 'button.a', function(event) {
-      audioElementA.load(); audioElementA.play();
-  });
-  $('.instrument').on('click', 'button.b', function(event) {
-      audioElementB.load(); audioElementB.play();
+
+
+  $('body').keypress(function(event) {
+    var media;
+
+    switch (event.keyCode) {
+      case 49:
+        media = 'media/c_note.mp3';
+        break;
+      case 50:
+        media = 'media/d_note.mp3';
+        break;
+      case 51:
+        media = 'media/e_note.mp3';
+        break;
+      case 52:
+        media = 'media/f_note.mp3';
+        break;
+      case 53:
+        media = 'media/g_note.mp3';
+        break;
+      case 54:
+        media = 'media/a_note.mp3';
+        break;
+      case 55:
+        media = 'media/b_note.mp3';
+        break;
+      }
+
+      var audioElement = document.createElement('audio');
+      if (media) {
+        audioElement.setAttribute('src', media);
+        audioElement.load(); audioElement.play();
+      }
+
   });
 
 });
