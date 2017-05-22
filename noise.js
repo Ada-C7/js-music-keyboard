cb0f5db07b2714e4fadab97cd624eefa88cc4568
Theresa Manney <theresa.m.17@gmail.com>
2017-05-22 14:43:21 -0700

    All letters will play when clicked on

--- noise.js
+++ noise.js
@@ -1,3 +1,31 @@
 $(document).ready( function() {
   // your code here
+
+  $(".c").click(function() {
+    document.getElementById("cAudio").load().play();
+  });
+
+  $(".d").click(function() {
+    document.getElementById("dAudio").play();
+  });
+
+  $(".e").click(function() {
+    document.getElementById("eAudio").play();
+  });
+
+  $(".f").click(function() {
+    document.getElementById("fAudio").play();
+  });
+
+  $(".g").click(function() {
+    document.getElementById("gAudio").play();
+  });
+
+  $(".a").click(function() {
+    document.getElementById("aAudio").play();
+  });
+
+  $(".b").click(function() {
+    document.getElementById("bAudio").play();
+  });
 });
