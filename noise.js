7bd251e9083b2c9fd3aaad94f15579af59934efd
jjousun <jjousun@gmail.com>
2017-05-22 16:23:38 -0700

    Functions for keydown and click

--- noise.js
+++ noise.js
@@ -1,3 +1,22 @@
 $(document).ready( function() {
-  // your code here
+  $('.instrument').on('click', 'button', function(event) {
+    var note = $(this).html();
+    var audio = document.getElementById(note + "Audio");
+
+    audio.load();
+    audio.play();
+  });
+
+  $('body').keydown(function(event){
+    var keys = {'c': 'true', 'd': 'true', 'e': 'true', 'f': 'true', 'g': 'true', 'a': 'true', 'b': 'true'};
+
+    if(keys[event.key]) {
+      var note = event.key;
+      var audio = document.getElementById(note + "Audio");
+
+      audio.load();
+      audio.play();
+    }
+  });
+
 });
