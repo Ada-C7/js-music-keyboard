525b7c87d5b1bf2003f08709d9e3dd6747bbb721
add2point71dots <alcland@gmail.com>
2017-05-22 15:21:39 -0700

    refactored with a helper function

--- noise.js
+++ noise.js
@@ -2,24 +2,28 @@ $(document).ready( function() {
   var notes = ['c', 'd', 'e', 'f', 'g', 'a', 'b'];
 
   var playNote = function(note) {
+    var audio = $("#" + note + "Audio");
+    audio.trigger('load');
+    audio.trigger('play');
+  };
+
+  var playNoteHelper = function(note) {
     return function() {
-      var audio = $("#" + note + "Audio");
-      audio.trigger('load');
-      audio.trigger('play');
+      playNote(note);
     };
   };
 
   var setClickPlay = function() {
     for (var i = 0; i < notes.length; i++) {
       var note = notes[i];
-      $('.' + note).on('click', playNote(note));
+      $('.' + note).on('click', playNoteHelper(note));
     }
   };
 
   $('body').keydown(function(event) {
     console.log(event.key);
     if (notes.includes(event.key)) {
-      playNote(event.key)();
+      playNote(event.key);
     }
   });
 
