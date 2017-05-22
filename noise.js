35aeb80bf47bb6918db64070ec53778cf8ed07b1
Ann Dai <gofarann@gmail.com>
2017-05-22 15:05:20 -0700

    added playNote function

--- noise.js
+++ noise.js
@@ -1,9 +1,46 @@
-$(document).ready( function() {
-  
 
- $('.c').click(function() {
-   var note = $("#cAudio")[0];
-   note.play();
- });
+var playNote = function(note){
+   var sound = $("#" + note +"Audio")[0];
+   sound.play();
+ };
 
-});
+
+$(document).ready(function() {
+
+   $('.c').click(function() {
+     note = "c";
+     playNote(note);
+   });
+
+   $('.d').click(function() {
+     note = "d";
+     playNote(note);
+   });
+
+   $('.e').click(function() {
+     note = "e";
+     playNote(note);
+   });
+
+   $('.f').click(function() {
+     note = "f";
+     playNote(note);
+   });
+
+   $('.g').click(function() {
+     note = "g";
+     playNote(note);
+   });
+
+   $('.a').click(function() {
+     note = "a";
+     playNote(note);
+   });
+
+   $('.b').click(function() {
+     note = "b";
+     playNote(note);
+   });
+
+}
+);
