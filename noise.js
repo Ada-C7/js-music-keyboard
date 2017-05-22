2edcef615becff4229e4fa7edff277b82f78dd93
Anacelia Gillette <acgillette426@gmail.com>
2017-05-22 14:38:54 -0700

    Finished keyboard

--- noise.js
+++ noise.js
@@ -1,3 +1,15 @@
 $(document).ready( function() {
-  // your code here
+  $('button').click(function() {
+    note = $(this).html();
+    audio = '#' + note + 'Audio';
+    $(audio).get(0).play();
+  });
+
+  $('body').keydown(function(event) {
+    var notes = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+    if (notes.indexOf(event.key) !== -1) {
+      audio = '#' + event.key + 'Audio';
+      $(audio).get(0).play();
+    }
+  });
 });
