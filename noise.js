9c0944650b80e9a270e0855a983a84612042fe1d
Haby Randall <sow.haby@gmail.com>
2017-05-22 16:00:32 -0700

    Wrote keydown and click functions, now working on refactoring and drying

--- noise.js
+++ noise.js
@@ -1,3 +1,39 @@
 $(document).ready( function() {
-  // your code here
+  var playing = false;
+
+
+  $('button').click(function() {
+    var note = $(this).html();
+    console.log(note);
+    var sound = '#' + note + 'Audio';
+    $(sound).load();
+    $(sound).get(0).play();
+
+    //
+    // if (playing === false) {
+    //   $('#cAudio').trigger('play');
+    //   playing = true;
+    //   $(this).text("stop sound");
+    // } else {
+    //   document.getElementById('#cAudio').pause();
+    //   playing = false;
+    //   $(this).text("restart sound");
+    //
+    //   $(this).play();
+    //   alert('music playing');
+    //
+    // }
+    // console.log(button);
+  });
+
+  $('body').keydown(function(event) {
+      var keys = {'c': true, 'd': true, 'e': true, 'f': true, 'g': true, 'a': true, 'b': true};
+    if (keys[event.key]) {
+      var note = event.key;
+      console.log(note);
+      var sound = '#' + note + 'Audio';
+      $(sound).load();
+      $(sound).get(0).play();
+    }
+  });
 });
