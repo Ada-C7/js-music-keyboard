85de2b00d1b5237ab387c4c42c1c7cf8fcd1fa81
Ann Dai <gofarann@gmail.com>
2017-05-22 15:36:08 -0700

    used class of note in on click function

--- noise.js
+++ noise.js
@@ -4,43 +4,11 @@ var playNote = function(note){
    sound.play();
  };
 
-
 $(document).ready(function() {
 
-   $('.c').click(function() {
-     note = "c";
-     playNote(note);
-   });
-
-   $('.d').click(function() {
-     note = "d";
-     playNote(note);
-   });
-
-   $('.e').click(function() {
-     note = "e";
-     playNote(note);
-   });
-
-   $('.f').click(function() {
-     note = "f";
-     playNote(note);
-   });
-
-   $('.g').click(function() {
-     note = "g";
-     playNote(note);
-   });
-
-   $('.a').click(function() {
-     note = "a";
-     playNote(note);
-   });
-
-   $('.b').click(function() {
-     note = "b";
-     playNote(note);
-   });
+  $(".note").on('click', function(){
+    playNote($(this).html());
+  });
 
 }
 );
