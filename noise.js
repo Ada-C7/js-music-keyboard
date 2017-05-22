fad32ea37a91ee38bc4c11e7ccab87f2f20254cc
Kelly Souza <kellysouza99@gmail.com>
2017-05-22 15:58:26 -0700

    Calls source for audio rather than direct to file

--- noise.js
+++ noise.js
@@ -5,32 +5,30 @@ $(document).ready(function() {
 
     switch (event.keyCode) {
       case 49:
-        media = 'media/c_note.mp3';
+        media = $('#cAudio')[0];
         break;
       case 50:
-        media = 'media/d_note.mp3';
+        media = $('#dAudio')[0];
         break;
       case 51:
-        media = 'media/e_note.mp3';
+        media = $('#eAudio')[0];
         break;
       case 52:
-        media = 'media/f_note.mp3';
+        media = $('#fAudio')[0];
         break;
       case 53:
-        media = 'media/g_note.mp3';
+        media = $('#gAudio')[0];
         break;
       case 54:
-        media = 'media/a_note.mp3';
+        media = $('#aAudio')[0];
         break;
       case 55:
-        media = 'media/b_note.mp3';
+        media = $('#bAudio')[0];
         break;
       }
 
-      var audioElement = document.createElement('audio');
       if (media) {
-        audioElement.setAttribute('src', media);
-        audioElement.load(); audioElement.play();
+        media.load(); media.play();
       }
 
   });
