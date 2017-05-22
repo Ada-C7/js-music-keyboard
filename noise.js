00b2e8fd5826241d9fc2fdea2918d4c364af7133
Brenna <bcmdarroch@gmail.com>
2017-05-22 15:03:53 -0700

    did the thing yeeeah

--- noise.js
+++ noise.js
@@ -1,3 +1,9 @@
 $(document).ready( function() {
-  // your code here
+  $('.instrument').on ('click', '.note', function() {
+    var note = $(this).html();
+    var audio = '#' + note + "Audio";
+    console.log(note);
+    $(audio)[0].load();
+    $(audio)[0].play();
+  });
 });
