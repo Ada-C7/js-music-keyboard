c518927bb639424f5208141c320e1e99cd635048
Kelly Souza <kellysouza99@gmail.com>
2017-05-22 15:16:15 -0700

    Cleans up old commented out code

--- noise.js
+++ noise.js
@@ -1,29 +1,5 @@
 $(document).ready(function() {
 
-  // var audioElementC = document.createElement('audio');
-  //   audioElementC.setAttribute('src', 'media/c_note.mp3');
-  //
-  // var audioElementD = document.createElement('audio');
-  //   audioElementD.setAttribute('src', 'media/d_note.mp3');
-  //
-  // var audioElementE = document.createElement('audio');
-  //   audioElementE.setAttribute('src', 'media/e_note.mp3');
-  //
-  // var audioElementF = document.createElement('audio');
-  //   audioElementF.setAttribute('src', 'media/f_note.mp3');
-  //
-  // var audioElementG = document.createElement('audio');
-  //   audioElementG.setAttribute('src', 'media/g_note.mp3');
-  //
-  // var audioElementA = document.createElement('audio');
-  //   audioElementA.setAttribute('src', 'media/a_note.mp3');
-  //
-  // var audioElementB = document.createElement('audio');
-  //   audioElementB.setAttribute('src', 'media/b_note.mp3');
-
-
-
-
   $('body').keypress(function(event) {
     var media;
 
