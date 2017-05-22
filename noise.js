1313876c9e5f2de61df99e9af536a0a512fb115d
Kayla <kubicke.kayla@gmail.com>
2017-05-22 15:30:03 -0700

    Audio clip plays when button is clicked.

--- noise.js
+++ noise.js
@@ -1,3 +1,22 @@
 $(document).ready( function() {
-  // your code here
+
+  console.log(event);
+
+  // Grabbing the audio source. (Working)
+  var audioC = document.getElementById("cAudio");
+  var audioD = document.getElementById("dAudio");
+
+
+  var playNote = function() {
+    audioC.play();
+  };
+
+  // var pauseNote = function() {
+  //   audioC.pause();
+  // };
+
+  $('.instrument').on('click', 'button', function(event) {
+    console.log(event);
+    $(this).addClass(playNote());
+  });
 });
