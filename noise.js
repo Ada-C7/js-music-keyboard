72c08bfcba28880aeef0640a99573f99b9b18044
Bo Trethewey <botrethewey@gmail.com>
2017-05-22 15:17:05 -0700

    added code for click play

--- noise.js
+++ noise.js
@@ -1,3 +1,20 @@
-$(document).ready( function() {
-  // your code here
+$(document).ready(function() {
+  // handles click playing
+  $('.note').on('click', playByClick);
 });
+
+var playByClick = function() {
+  // store the actual key value from the clicked .note element
+  var keyClicked = $(this).html();
+  // send that key to play function
+  play(keyClicked);
+};
+
+var play = function(key) {
+  // created a string for the audio element, using the passed key info
+  var audio = "#" + key + "Audio";
+  // .load() allows repeated play
+  $(audio)[0].load();
+  // .play() plays the audio from html
+  $(audio)[0].play();
+};
