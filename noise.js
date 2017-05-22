535572e2d5a0c2a356c5d3b9e936941ad3227a25
Theresa Manney <theresa.m.17@gmail.com>
2017-05-22 15:44:42 -0700

    Note will replay when you click it multiple times, starting to clean up play and load functionality

--- noise.js
+++ noise.js
@@ -1,11 +1,20 @@
 $(document).ready( function() {
   // your code here
+  var replayNote = function(note) {
+    key = document.getElementById(note + "Audio");
+    key.load();
+    key.play();
+  };
 
-  $(".c").click(function() {
-    document.getElementById("cAudio").load().play();
+  $('.c').click(function() {
+    replayNote("c");
   });
 
+  
+
   $(".d").click(function() {
+    document.getElementById("dAudio").load();
+
     document.getElementById("dAudio").play();
   });
 
