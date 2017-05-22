ec7de2a5df73b6904448eff0d173b0f0653f4195
sai samant <saisamant@gmail.com>
2017-05-22 15:47:55 -0700

    plays note when clicked; uses closure to set up event handlers for each note

--- noise.js
+++ noise.js
@@ -1,3 +1,45 @@
 $(document).ready( function() {
-  // your code here
+
+function playAudio(letter) {
+    console.log("HELLO");
+    var noteID = ("#" + letter + "Audio");
+    console.log(noteID);
+    $("#" + letter + "Audio")[0].load();
+    $("#" + letter + "Audio")[0].play();
+}
+
+function helper(theNote) {
+  return function() {
+    playAudio(theNote);
+  };
+}
+
+function setupNotes() {
+
+  notes = ["a", "b", "c", "d", "e", "f", "g"];
+
+  for (var i = 0; i < notes.length; i++) {
+    // We get the help element in the Array
+    var note = notes[i];
+      //  When this element gets focus
+
+      $("." + note).click(helper(note));
+      console.log("Event Handler: " + note);
+
+  }
+}
+
+setupNotes();
+
+  //
+  // $(".c").click(function(){
+  //   console.log("C");
+  //   console.log($('#cAudio'));
+  //   $("#cAudio")[0].load();
+  //   $("#cAudio")[0].play();
+  //
+  // });
+
+  //set up on clicks so that the right note plays when you click on the corresponding button
+
 });
