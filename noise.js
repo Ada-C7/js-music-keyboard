d14fce84d87b31ceb280c45b361e429d74d5f750
Aurora Lemieux <aurora.lemieux@gmail.com>
2017-05-22 14:25:14 -0700

    click and type functionality working to play notes

--- noise.js
+++ noise.js
@@ -1,3 +1,23 @@
 $(document).ready( function() {
-  // your code here
+
+  // trigger appropriate sound when box is clicked
+  $(".instrument").on("click", "button", function() {
+    var note = $(this).html();
+    console.log(note);
+    var audio = document.getElementById(note + "Audio");
+    audio.play();
+  });
+
+  // trigger appropriate sound when key is pressed
+  $("body").keydown(event, function() {
+    var allNotes = ["a", "b", "c", "d", "e", "f", "g"];
+    if (allNotes.includes(event.key)) {
+      var note = event.key;
+      var audio = document.getElementById(note + "Audio");
+      audio.play();
+    } else {
+      alert("Not a valid key, dodo");
+    }
+  });
+
 });
