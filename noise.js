f7b98d0f67d6b58e0a7523e07faa454a75be53f9
Regan Huff <regan.huff@gmail.com>
2017-05-22 14:24:45 -0700

    Initial attempt at closure structure

--- noise.js
+++ noise.js
@@ -1,3 +1,34 @@
 $(document).ready( function() {
   // your code here
+  // click on a box (class note)
+  // trigger a tone (class a, b, etc.)
+  // cycle through all the tones
+  // bind calling play on a particular audio embed to clicking on the appropriate box
 });
+
+// <button onclick="document.getElementById('myTune').play()">Play Music</button>
+
+
+function setupClicks() {
+  var tones = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+
+  for (var i = 0; i < tones.length; i++) {
+    var tone = tones[i];
+    //make a function to attach to the appropriate click event
+    []
+  }
+};
+
+function makeTone(tone) {
+  var audio = $("#" + tone + "Audio");
+  $("." + tone).click(function() {
+    audio.play();
+  })
+}
+
+
+function makeToneCallback(tone) {
+  return function() {
+    makeTone(tone);
+  }
+}
