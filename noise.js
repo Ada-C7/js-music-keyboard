16cc5ced7f9007e848635c80b29b4fdadd87dad4
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-05-22 14:10:29 -0700

    set up click event handling for each keyboard button

--- noise.js
+++ noise.js
@@ -1,3 +1,31 @@
 $(document).ready( function() {
   // your code here
+  $(".note c").click(function(){
+    audio.play();
+  });
+
+  $(".note d").click(function(){
+    audio.play();
+  });
+
+  $(".note e").click(function(){
+    audio.play();
+  });
+
+  $(".note f").click(function(){
+    audio.play();
+  });
+
+  $(".note g").click(function(){
+    audio.play();
+  });
+
+  $(".note a").click(function(){
+    audio.play();
+  });
+
+  $(".note b").click(function(){
+    audio.play();
+  });
+
 });
