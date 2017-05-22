f2723edb6d545a922be03b86a88e0d8e44d81b14
add2point71dots <alcland@gmail.com>
2017-05-22 14:59:14 -0700

    can play all the notes on click

--- noise.js
+++ noise.js
@@ -1,3 +1,22 @@
 $(document).ready( function() {
-  // your code here
+
+  var clickPlay = function(note) {
+    var playNote = function() {
+      var audio = $("#" + note + "Audio");
+      audio.trigger('load');
+      audio.trigger('play');
+    };
+    return playNote;
+  };
+
+  var setClickPlay = function() {
+    var notes = ['c', 'd', 'e', 'f', 'g', 'a', 'b'];
+
+    for (var i = 0; i < notes.length; i++) {
+      var note = notes[i];
+      $('.' + note).on('click', clickPlay(note));
+    }
+  };
+
+  setClickPlay();
 });
