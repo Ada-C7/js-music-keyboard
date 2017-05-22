3594dba6c0bfa4984fcdd9f7117d533e35fbb657
Ashtn <ashtonehorton@gmail.com>
2017-05-22 16:38:28 -0700

    added events that will play sound when element is clicked, and when the associcated key is pressed

--- noise.js
+++ noise.js
@@ -1,3 +1,24 @@
 $(document).ready( function() {
-  // your code here
+
+
+//on the click of .note and .(letter of class)
+$('button').click(function(){
+note = $(this).html();
+// document.getElementById(note+"Audio").play();
+// play the id of ('letter of class'+'Audio')
+
+$('#' + note + "Audio").get(0).load();
+$('#' + note + "Audio").get(0).play();
+
+
+});
+
+$('body').keydown(function(event){
+// event.key returns the key that is pressed
+note = event.key;
+$('#' + note + "Audio").get(0).load();
+$('#' + note + "Audio").get(0).play();
+
+});
+
 });
