491ab2dd6cfd554c6f608aca6754571f17315c69
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-05-22 15:20:36 -0700

    removed global variable/method for contains

--- noise.js
+++ noise.js
@@ -1,14 +1,27 @@
 $(document).ready( function() {
 
-  Array.prototype.contains = function ( needle ) {
-     for(i in this) {
-         if (this[i] == needle) return true;
-     }
-     return false;
-  };
-
-  button.click(function(){
+  $(".d").click(function(){
     playKey(this.class);
+  });
+
+  $(".e").click(function(){
+    playKey('e');
+  });
+
+  $(".f").click(function(){
+    playKey('f');
+  });
+
+  $(".g").click(function(){
+    playKey('g');
+  });
+
+  $(".a").click(function(){
+    playKey('a');
+  });
+
+  $(".b").click(function(){
+    playKey('b');
   });
 
   $('body').keydown(function(event){
@@ -17,7 +30,7 @@ $(document).ready( function() {
 
   var playKey = function(key){
     var validKeys = ['c', 'd', 'e', 'f', 'g', 'a', 'b'];
-    if (validKeys.contains(key)) {
+    if (validKeys.indexOf(key) > -1) {
       document.getElementById(key + "Audio").play();
     }
   };
