81b03a2535c2daeb7ffa4c418b3c76d6e35043cc
Chantal Rollison <rollison.chantal@gmail.com>
2017-05-22 16:19:12 -0700

    buttons work!

--- noise.js
+++ noise.js
@@ -1,3 +1,22 @@
 $(document).ready( function() {
-  // your code here
+
+  function makeNoiser(noise) {
+    return function() {
+      $('.instrument').html(noise);
+    };
+  }
+
+  function play(event) {
+    var note = $(this).attr("id");
+    document.getElementById(note + "Audio").play();
+  }
+
+  // var soundD = new Audio("media/d_note.wav");
+  // var soundE = new Audio("media/e_note.wav");
+  // var soundF = new Audio("media/f_note.wav");
+  // var soundG = new Audio("media/g_note.wav");
+  // var soundA = new Audio("media/a_note.wav");
+  // var soundB = new Audio("media/b_note.wav");
+
+  $('.note').on("click", play);
 });
