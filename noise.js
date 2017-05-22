762584359dd15ada2aa260b2718cfac0d5a6c8a3
Lynn Trickey <lynn.trickey@gmail.com>
2017-05-22 14:15:39 -0700

    Added a method to play sounds even if sound had not been finished

--- noise.js
+++ noise.js
@@ -1,5 +1,22 @@
 $(document).ready( function() {
+
+  var alwaysPlay = function (elementID) {
+    var audio = document.getElementById(elementID);
+    if (audio.paused) {
+        audio.play();
+    } else {
+      audio.currentTime = 0;
+    }
+  };
+
   $(".c").click(function() {
-    document.getElementById("cAudio").play();
+    alwaysPlay("cAudio");
   });
+  $(".d").click(function() {
+    document.getElementById("dAudio").play();
+  });
+  // trying to do this all in one rather than one for each.
+  // $('.instrument').on('click', 'button', function(event) {
+  //   document.getElementById("Audio").play();
+  // });
 });
