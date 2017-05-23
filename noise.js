05753b22b1793990592774be324dae67eca8e61c
Tamiko Terada <tamikodevo@gmail.com>
2017-05-23 08:23:16 -0700

    keyboard input visuals

--- noise.js
+++ noise.js
@@ -1,21 +1,41 @@
+
 $(document).ready(function() {
 
   var play = function(note) {
     var sound = $("#"+note+"Audio")[0];
+    dampen();
     sound.load();
     sound.play();
   };
 
   // play notes by clicking
   $("button").click(function(){
-    var note = this.className.slice(-1);
+    var note = this.innerHTML;
     play(note);
   });
 
   // play notes by typing
-  $("body").keydown(function(event){
+  $("body").keypress(function(event){
     var input_ascii = event.which;
-    var letter = String.fromCharCode(input_ascii);
-    play(letter.toLowerCase());
+    var letter = (String.fromCharCode(input_ascii)).toLowerCase();
+    $("."+letter).addClass("active");
+    // $("."+letter).addClass("active").delay(5000).removeClass("active");
+    setTimeout(function(){
+      $("."+letter).removeClass("active");
+    }, 150);
+    play(letter);
   });
+
+  // dampen sounds between each one
+  var dampen = function(){
+    var notes = "abcdefg";
+    for (var i = 0; i < notes.length; i ++) {
+      pause(notes[i]);
+    }
+  };
+
+  var pause = function(note) {
+    var sound = $("#"+note+"Audio")[0];
+    sound.pause();
+  };
 });
