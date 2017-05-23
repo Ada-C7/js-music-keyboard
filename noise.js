7a78fb86e4646c8c494255f2ac38f7c75eecdb8e
Kayla <kubicke.kayla@gmail.com>
2017-05-22 19:40:53 -0700

    Refactored code to create Note object.
    An individual sound is played when button is clicked.
    During a click, a Note object is created.

--- noise.js
+++ noise.js
@@ -1,60 +1,30 @@
 $(document).ready( function() {
 
-  console.log(event);
 
-  // Grabbing the audio source. (Working)
+  var Note = function(letter) {
+    this.letter = letter;
+  };
 
-  var audioC = document.getElementById("cAudio");
-  var audioD = document.getElementById("dAudio");
-  var audioE = document.getElementById("eAudio");
-  var audioF = document.getElementById("fAudio");
-  var audioG = document.getElementById("gAudio");
-  var audioA = document.getElementById("aAudio");
-  var audioB = document.getElementById("bAudio");
+  Note.prototype = {
+    location: function() {
+      var tag = this.letter + "Audio";
 
+      var audioLocation = document.getElementById(tag);
+      console.log(audioLocation);
+      return audioLocation;
+    },
 
-  var playNote = function(note) {
-    if (note === 'c') {
-      audioC.currentTime = 0;
-      audioC.play();
-    } else if (note === 'd') {
-      audioD.currentTime = 0;
-      audioD.play();
-    } else if (note === 'e') {
-      audioE.currentTime = 0;
-      audioE.play();
-    } else if (note === 'f') {
-      audioF.currentTime = 0;
-      audioF.play();
-    } else if (note === 'g') {
-      audioG.currentTime = 0;
-      audioG.play();
-    } else if (note === 'a') {
-      audioA.currentTime = 0;
-      audioA.play();
-    } else if (note === 'b') {
-      audioB.currentTime = 0;
-      audioB.play();
+    playNote: function() {
+      var location = this.location();
+      console.log(typeof location === 'string');
+      location.currentTime = 0;
+      return location.play();
     }
   };
 
-  // var pauseNote = function() {
-  //   audioC.pause();
-  // };
-
   $('.instrument').on('click', 'button', function(event) {
-    console.log(event);
-    console.log($(this).html());
     var note = $(this).html();
-    $(this)(playNote(note));
+    var newNote = new Note(note);
+    $(this)(newNote.playNote());
   });
 });
-
-
-// if (this.paused == false) {
-//     this.pause();
-//     alert('music paused');
-// } else {
-//     this.play();
-//     alert('music playing');
-// }
