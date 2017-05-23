e737410b3e188bf976caa4e56225ebe55e02acb6
Kayla <kubicke.kayla@gmail.com>
2017-05-22 17:59:46 -0700

    Sound plays everytime a button is pressed.

--- noise.js
+++ noise.js
@@ -3,6 +3,7 @@ $(document).ready( function() {
   console.log(event);
 
   // Grabbing the audio source. (Working)
+
   var audioC = document.getElementById("cAudio");
   var audioD = document.getElementById("dAudio");
   var audioE = document.getElementById("eAudio");
@@ -14,30 +15,46 @@ $(document).ready( function() {
 
   var playNote = function(note) {
     if (note === 'c') {
+      audioC.currentTime = 0;
       audioC.play();
     } else if (note === 'd') {
+      audioD.currentTime = 0;
       audioD.play();
     } else if (note === 'e') {
+      audioE.currentTime = 0;
       audioE.play();
     } else if (note === 'f') {
+      audioF.currentTime = 0;
       audioF.play();
     } else if (note === 'g') {
+      audioG.currentTime = 0;
       audioG.play();
     } else if (note === 'a') {
+      audioA.currentTime = 0;
       audioA.play();
     } else if (note === 'b') {
+      audioB.currentTime = 0;
       audioB.play();
     }
   };
 
-  var pauseNote = function() {
-    audioC.pause();
-  };
+  // var pauseNote = function() {
+  //   audioC.pause();
+  // };
 
   $('.instrument').on('click', 'button', function(event) {
     console.log(event);
     console.log($(this).html());
     var note = $(this).html();
-    $(this).addClass(playNote(note));
+    $(this)(playNote(note));
   });
 });
+
+
+// if (this.paused == false) {
+//     this.pause();
+//     alert('music paused');
+// } else {
+//     this.play();
+//     alert('music playing');
+// }
