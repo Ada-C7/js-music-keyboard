9f32c6ac2bc7800103eebe062a718bc22a96c5ad
Kayla <kubicke.kayla@gmail.com>
2017-05-22 20:06:22 -0700

    Keyboard input can play notes.
    Note plays if corresponding key is pressed or button is clicked.

--- noise.js
+++ noise.js
@@ -25,4 +25,36 @@ $(document).ready( function() {
     var newNote = new Note(note);
     $(this)(newNote.playNote());
   });
+
+  $('body').keydown(function(event) {
+    // var note = $(this).html();
+    // console.log(note);
+
+    // if (event.key === note) {
+    //   var newNote = new Note(note);
+    //   $(this)(newNote.playNote());
+    // }
+    if (event.key === 'c') {
+      var newNote = new Note('c');
+      $(this)(newNote.playNote());
+    } else if (event.key === 'd') {
+      var newNote = new Note('d');
+      $(this)(newNote.playNote());
+    } else if (event.key === 'e') {
+      var newNote = new Note('e');
+      $(this)(newNote.playNote());
+    } else if (event.key === 'f') {
+      var newNote = new Note('f');
+      $(this)(newNote.playNote());
+    } else if (event.key === 'g') {
+      var newNote = new Note('g');
+      $(this)(newNote.playNote());
+    } else if (event.ket === 'a') {
+      var newNote = new Note('a');
+      $(this)(newNote.playNote());
+    } else if (event.key === 'b') {
+      var newNote = new Note('b');
+      $(this)(newNote.playNote());
+    }
+  });
 });
