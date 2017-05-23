723560f30828ad9146290f9bb454ed33a855f93b
janicewilson <janice.wilson@mac.com>
2017-05-23 07:36:37 -0700

    I've no idea why I'm blanking on all things github. Here's my singular commit. Sigh.

--- noise.js
+++ noise.js
@@ -1,3 +1,47 @@
-$(document).ready( function() {
-  // your code here
+$(document).ready(function() {
+
+     var key = {
+          '.a': 'aAudio',
+          '.b': 'bAudio',
+          '.c': 'cAudio',
+          '.d': 'dAudio',
+          '.e': 'eAudio',
+          '.f': 'fAudio',
+          '.g': 'gAudio'
+     };
+
+     var note = '';
+     $('div').click(function(event) {
+          note = '.' + $(event.target).attr('class').slice(-1);
+          return note;
+     });
+
+     $('div').on('click', note, function(event) {
+          document.getElementById(key[note]).play();
+     });
+
+     $("body").keydown(function(event) {
+          if (event.key == '.' + '') {
+               alert('You got an A!');
+          }
+     });
+
+     $('body').keypress(function(event){
+          note = String.fromCharCode( event.which );
+          // console.log(note);
+          // console.log(typeof note);
+
+          return note
+     });
+
+     $("body").keydown(function(event) {
+          if (event.key == note) {
+               note = '.' + note;
+               console.log(note);
+
+               document.getElementById(key[note]).play();
+          }
+     });
+
+
 });
