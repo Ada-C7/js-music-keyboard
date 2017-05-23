4c72aa7a16aa75ffbfe8b46985baf036292efe4f
Chantal Rollison <rollison.chantal@gmail.com>
2017-05-22 18:38:33 -0700

    it now works for keystrokes!

--- noise.js
+++ noise.js
@@ -1,22 +1,14 @@
+function play(event) {
+  var note = $(this).attr("id");
+  document.getElementById(note + "Audio").play();
+}
+
 $(document).ready( function() {
-
-  function makeNoiser(noise) {
-    return function() {
-      $('.instrument').html(noise);
-    };
-  }
-
-  function play(event) {
-    var note = $(this).attr("id");
-    document.getElementById(note + "Audio").play();
-  }
-
-  // var soundD = new Audio("media/d_note.wav");
-  // var soundE = new Audio("media/e_note.wav");
-  // var soundF = new Audio("media/f_note.wav");
-  // var soundG = new Audio("media/g_note.wav");
-  // var soundA = new Audio("media/a_note.wav");
-  // var soundB = new Audio("media/b_note.wav");
-
-  $('.note').on("click", play);
+  // $('.note').on("click", play);
+  $("body").keydown(function(event) {
+    var key = document.getElementById(event.key + "Audio");
+    if (key){
+      key.play();
+    }
+  });
 });
