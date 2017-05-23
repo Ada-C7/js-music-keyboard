35c8cfa12aef8e8da9db28c30f996b1c005df2f7
Ann Dai <gofarann@gmail.com>
2017-05-23 15:54:30 -0700

    added trip detail appends to index

--- noise.js
+++ noise.js
@@ -1,6 +1,7 @@
 
 var playNote = function(note){
    var sound = $("#" + note +"Audio")[0];
+  //  loads sound so same key can be played repeatedly
    sound.preload = 'auto';
    sound.load();
    sound.play();
