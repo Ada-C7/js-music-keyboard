9226e8bae62507f88027f5082ca0af832ebadd33
Tamiko Terada <tamikodevo@gmail.com>
2017-05-23 10:45:09 -0700

    to submit

--- noise.js
+++ noise.js
@@ -19,21 +19,20 @@ $(document).ready(function() {
     var input_ascii = event.which;
     var letter = (String.fromCharCode(input_ascii)).toLowerCase();
     $("."+letter).addClass("active");
-    // $("."+letter).addClass("active").delay(5000).removeClass("active");
+
     setTimeout(function(){
       $("."+letter).removeClass("active");
     }, 120);
     play(letter);
   });
 
-  // dampen sounds between each one
+  // to dampen sounds between each one
   var dampen = function(){
     var notes = "abcdefg";
     for (var i = 0; i < notes.length; i ++) {
       pause(notes[i]);
     }
   };
-
   var pause = function(note) {
     var sound = $("#"+note+"Audio")[0];
     sound.pause();
