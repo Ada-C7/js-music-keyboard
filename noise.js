34c5bf1479e648d3df075405ba8793948fccdea5
Olivia <ssecretsharer@gmail.com>
2017-05-22 20:24:48 -0700

    Baseline working in safari. Spent much time trying to get chrome settings to play video/not bug out with this project

--- noise.js
+++ noise.js
@@ -1,3 +1,23 @@
 $(document).ready( function() {
-  // your code here
+
+
+$(".instrument").on("click", "button", function() {
+  var note = $(this).html();
+  console.log(note);
+  var audio = document.getElementById(note + "Audio");
+  audio.play();
+});
+
+
+  $('body').keypress(function(event){
+    var scale = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+    var note = event.key;
+    var audio = '#' + note + "Audio";
+
+    if (scale.includes(note)) {
+      $(audio)[0].load();
+      $(audio)[0].play();
+    }
+
+  });
 });
