817015ecaa119a52c7ccc39897b7d796bc7cde40
Lynn Trickey <lynn.trickey@gmail.com>
2017-05-22 19:28:29 -0700

    finishing up to submit

--- noise.js
+++ noise.js
@@ -1,28 +1,27 @@
 $(document).ready( function() {
 
-var alwaysPlay = function (elementID) {
-  var audio = document.getElementById(elementID);
-  if (audio.paused) {
-      audio.play();
-  } else {
-    audio.currentTime = 0;
-  }
-};
-
-var helper = function(elementAudioID) {
-  return function() {
-    alwaysPlay(elementAudioID);
-  };
-};
-
-var keyPressHelper = function(element) {
-  return function(event) {
-    console.log(element);
-    if (event.key == element.key) {
-      alwaysPlay(element.audio);
+  var alwaysPlay = function (elementID) {
+    var audio = document.getElementById(elementID);
+    if (audio.paused) {
+        audio.play();
+    } else {
+      audio.currentTime = 0;
     }
   };
-};
+
+  var clickHelper = function(elementAudioID) {
+    return function() {
+      alwaysPlay(elementAudioID);
+    };
+  };
+
+  var keyPressHelper = function(element) {
+    return function(event) {
+      if (event.key == element.key) {
+        alwaysPlay(element.audio);
+      }
+    };
+  };
 
   var setupKeyboard = function() {
     var keys = [
@@ -37,53 +36,11 @@ var keyPressHelper = function(element) {
 
     for (var i = 0; i < keys.length; i++) {
       var selected = keys[i];
-      $('.' + selected.key).click(helper(selected.audio));
+      $('.' + selected.key).click(clickHelper(selected.audio));
+      // Would be good to take out of loop eventually.
       $('body').keydown(keyPressHelper(selected));
     }
   };
 
   setupKeyboard();
-
-  $("body").keydown(function() {
-    if (event.key == 'c') {
-      alwaysPlay("cAudio");
-    }
-  });
-
-//   $("body").keydown(function() {
-//     if (event.key == 'd') {
-//       alwaysPlay("dAudio");
-//     }
-//   });
-//
-//   $("body").keydown(function() {
-//     if (event.key == 'e') {
-//       alwaysPlay("eAudio");
-//     }
-//   });
-//
-//   $("body").keydown(function() {
-//     if (event.key == 'f') {
-//       alwaysPlay("fAudio");
-//     }
-//   });
-//
-//   $("body").keydown(function() {
-//     if (event.key == 'g') {
-//       alwaysPlay("gAudio");
-//     }
-//   });
-//
-//   $("body").keydown(function() {
-//     if (event.key == 'a') {
-//       alwaysPlay("aAudio");
-//     }
-//   });
-//
-//   $("body").keydown(function() {
-//     if (event.key == 'b') {
-//       alwaysPlay("bAudio");
-//     }
-//   });
-
 });
