8f0938d6c8450823036f2bb3d5177d2688d65592
AlisonZ <alisonkzerbe@gmail.com>
2017-05-23 19:26:29 -0700

    audio working with click function

--- noise.js
+++ noise.js
@@ -1,3 +1,11 @@
 $(document).ready( function() {
-  // your code here
+    // curious how to DRY this up. i want to make a key variable and a keyAudio variable that will be set on click
+    //  with two variables, only have to write  this function once
+    $('body').click(function(event) {
+        var target = (event.target.classList[1]);
+        var key = target + "Audio"
+        console.log(key);
+
+        document.getElementById(key).play();
+    })
 });
